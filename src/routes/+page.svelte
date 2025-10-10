b7aeb5abddf6f70723a09322178f807fe01d62c7
nijasc <github.moonshine694@passmail.net>
2025-10-10 11:50:55 +0200

    Fixed taskbar overlap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,7 +38,7 @@
 
 {#if currentApp}
 	{@const App = currentApp.component}
-	<div class="h-full overflow-y-hidden w-full px-4 pt-12 pb-20">
+	<div class="h-full overflow-y-hidden w-full px-4 pt-12 pb-32">
 		<App props={currentProps} />
 	</div>
 {/if}
